f40340e6ee852197e6c3673ec0cf77081c36621b
alif2815 <bintangcaca2815@email.com>
2026-07-27 16:37:55 +0700

    feat:feature module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { MediaModule } from './media/media.module';
+import { NewsModule } from './news/news.module';
+import { EventModule } from './event/event.module';
+import { EbookModule } from './ebook/ebook.module';
+import { ShortLinkModule } from './short-link/short-link.module';
+import { VotingModule } from './voting/voting.module';
+import { AchievementModule } from './achievement/achievement.module';
+import { PeriodModule } from './period/period.module';
+import { PositionModule } from './position/position.module';
+import { StructureModule } from './structure/structure.module';
+import { FormSettingModule } from './form-setting/form-setting.module';
+import { RegistrationModule } from './registration/registration.module';
+import { EbookModule } from './ebook/ebook.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UserModule, MediaModule, NewsModule, EventModule, EbookModule, AchievementModule, PeriodModule, PositionModule, StructureModule, FormSettingModule, RegistrationModule, VotingModule, ShortLinkModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/voting/voting.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { VotingService } from './voting.service';
+import { CreateVotingDto } from './dto/create-voting.dto';
+import { UpdateVotingDto } from './dto/update-voting.dto';
+
+@Controller('voting')
+export class VotingController {
+  constructor(private readonly votingService: VotingService) {}
+
+  @Post()
+  create(@Body() createVotingDto: CreateVotingDto) {
+    return this.votingService.create(createVotingDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.votingService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.votingService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateVotingDto: UpdateVotingDto) {
+    return this.votingService.update(+id, updateVotingDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.votingService.remove(+id);
+  }
+}
